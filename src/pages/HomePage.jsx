f0ecadbf0fdc92d3bfc95da7bf3c95889525e60c
Adel <adelnmo2003@gmail.com>
2024-08-28 00:08:51 +0330

    add hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <Hero />
       home page
     </div>
   );
