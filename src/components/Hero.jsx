f7612d37d57ae44494aaa12704773b7c36ddc58b
Adel <adelnmo2003@gmail.com>
2024-08-28 00:18:47 +0330

    update svg to jsx format

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,13 +11,15 @@ function Hero() {
         <button type="submit">
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            fill="none"
             viewBox="0 0 24 24"
-            fill="currentColor"
+            strokeWidth={1.5}
+            stroke="currentColor"
           >
             <path
-              fill-rule="evenodd"
-              d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-              clip-rule="evenodd"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
             />
           </svg>
         </button>
