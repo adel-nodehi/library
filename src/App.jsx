b97a5deab92a9a9ab83008e02e67e729af3d1a0d
Adel <adelnmo2003@gmail.com>
2024-08-28 12:44:29 +0330

    use book context in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SavedPage from "./pages/SavedPage";
+import { BooksProvider } from "./assets/contexts/BooksContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/saved" element={<SavedPage />} />
-      </Routes>
-    </BrowserRouter>
+    <BooksProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/saved" element={<SavedPage />} />
+        </Routes>
+      </BrowserRouter>
+    </BooksProvider>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
+import { useBooks } from "../assets/contexts/BooksContext";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
 function HomePage() {
+  const state = useBooks();
+  console.log(state);
+
   return (
     <div>
       <Header />
       <Hero />
-      home page
     </div>
   );
 }
