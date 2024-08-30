fd5ef0d7682b709b94351e75e2a4e6abb038da2e
Adel <adelnmo2003@gmail.com>
2024-08-30 14:55:31 +0330

    implement book context

---
+++ src/contexts/BooksContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+
+const BooksContext = createContext();
+
+function BooksProvider({ children }) {
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const numFound = useRef(0);
+  console.log(books);
+
+  useEffect(function () {
+    async function fetchBooks() {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(
+          "https://openlibrary.org/search.json?title=the+lord+of+the+rings"
+        );
+
+        if (!res.ok) throw new Error("fetch error");
+
+        const data = await res.json();
+
+        numFound.current = data.numFound;
+
+        setBooks(() =>
+          data.docs.map((book) => {
+            return {
+              id: book.key,
+              title: book.title,
+              authors: book.author_name,
+              coverID: book.cover_i,
+              publishYear: book.first_publish_year,
+            };
+          })
+        );
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(true);
+      }
+    }
+
+    // fetchBooks();
+  }, []);
+
+  return (
+    <BooksContext.Provider
+      value={{
+        books,
+        isLoading,
+        setQuery,
+        numFound: numFound.current,
+      }}
+    >
+      {children}
+    </BooksContext.Provider>
+  );
+}
+
+function useBooks() {
+  const context = useContext(BooksContext);
+  if (context === undefined)
+    throw new Error("BooksContext was used outside BooksProvider");
+  return context;
+}
+
+export { BooksProvider, useBooks };
