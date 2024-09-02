cee2e279d833a41b8837cbf23d4513cbcb8f5875
Adel <adelnmo2003@gmail.com>
2024-09-02 19:24:36 +0330

    edit hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,37 +18,40 @@ function Hero() {
   }
 
   return (
-    <div className="hero">
-      <h1>find your book of choice.</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit, porro.
-      </p>
+    <div className="hero__container">
+      <div className="hero">
+        <h1>find your book of choice.</h1>
+        <p>
+          Lose yourself in a world of endless stories. Discover your next
+          favorite read, personalized just for you.
+        </p>
 
-      <form className="search-box" autoComplete="off" onSubmit={handleSubmit}>
-        <input
-          value={searchQuery}
-          onChange={handleChange}
-          type="search"
-          id="search"
-          placeholder="Search"
-          required
-        />
-        <button type="submit">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-            />
-          </svg>
-        </button>
-      </form>
+        <form className="search-box" autoComplete="off" onSubmit={handleSubmit}>
+          <input
+            value={searchQuery}
+            onChange={handleChange}
+            type="search"
+            id="search"
+            placeholder="Search"
+            required
+          />
+          <button type="submit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
