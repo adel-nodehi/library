84421bc1577826e8b8d7ff1ec9cea996c944c914
Adel <adelnmo2003@gmail.com>
2024-09-03 11:38:48 +0330

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <footer>&copy; copyright till end.</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__wrapper">
+        <span>&copy;</span> Copyright 2030 by Motahare.
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
