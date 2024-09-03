3e42326ebeb5bb2fc8a1253b2cbe9e6ff844e3dc
Adel <adelnmo2003@gmail.com>
2024-09-03 20:47:49 +0330

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <App />
-  // <StrictMode>
-  // </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
