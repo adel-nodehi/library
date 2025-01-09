defb718072d6b539d97b2171879d1596772be291
Adel Nodehi <adelnmo2003@gmail.com>
2025-01-09 16:17:26 +0330

    feat: move context provide to main file and provide AuthContext to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
 import SavedPage from "./pages/SavedPage";
-import { BooksProvider } from "./contexts/BooksContext";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <BooksProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/saved" element={<SavedPage />} />
-        </Routes>
-      </BrowserRouter>
-    </BooksProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/saved" element={<SavedPage />} />
+
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import React from "react";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { AuthProvider } from "./contexts/AuthContext";
+import { BooksProvider } from "./contexts/BooksContext";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BooksProvider>
+        <App />
+      </BooksProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
