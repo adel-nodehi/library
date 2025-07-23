fda18adc32c981106605f3c7a1a9c21b25810064
Adel Nodehi <136130809+adel-nodehi@users.noreply.github.com>
2025-07-23 14:03:55 +0330

    feat(theme): persist theme between reload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__wrapper">
-        <span>&copy;</span> Copyright 2030 by Motahare.
+        <span>&copy;</span> Copyright 2030.
       </div>
     </footer>
   );
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,27 +1,38 @@
-import { useState } from "react";
-import Button from "./Button";
+import { useState } from 'react';
+import Button from './Button';
 
 // Icons
-import sun from "../assets/images/sun.svg";
-import moon from "../assets/images/moon.svg";
+import sun from '../assets/images/sun.svg';
+import moon from '../assets/images/moon.svg';
 
 function Theme() {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(() => {
+    const isDark = localStorage.getItem('isThemeDark') === 'true';
+
+    if (!isDark) {
+      document.documentElement.removeAttribute('data-theme');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    }
+
+    return isDark;
+  });
 
   function handleThemeChange() {
     if (isDark) {
-      document.documentElement.removeAttribute("data-theme");
+      document.documentElement.removeAttribute('data-theme');
     } else {
-      document.documentElement.setAttribute("data-theme", "dark");
+      document.documentElement.setAttribute('data-theme', 'dark');
     }
 
     setIsDark((cur) => !cur);
+    localStorage.setItem('isThemeDark', !isDark);
   }
 
   return (
     <Button className="btn btn-theme" onClick={handleThemeChange}>
       <img src={isDark ? sun : moon} alt="theme icon" />
-      <p>{isDark ? "Light Mode" : "Dark Mode"}</p>
+      <p>{isDark ? 'Light Mode' : 'Dark Mode'}</p>
     </Button>
   );
 }
